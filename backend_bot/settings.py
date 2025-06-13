3d49cf4ef296360508a2d569b84b7742c28cdf20
GroverAEP <gespinozaprudencio@gmail.com>
2025-06-13 04:32:42 +0200

    actualizaicon commit 1

--- backend_bot/settings.py
+++ backend_bot/settings.py
@@ -128,7 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+# Static files (CSS, JavaScript)
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Media files (IMÁGENES SUBIDAS POR EL USUARIO)
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
